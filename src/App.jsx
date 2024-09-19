787f70be808d2fd2014ce21c70a3e8e1093a08cd
LuisVazquezPerez <luiscarlosvazquezperez@gmail.com>
2024-09-19 10:59:42 -0800

    Agregando local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Guitar from './components/Guitar'
@@ -7,12 +7,21 @@ import { db } from './data/db'
 function App() {
 
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]) //!ALMACENANDO TODO EN CART
+
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
 
 
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
     if (itemExists >= 0) { //EXISTE EN EL CARRITO
+      if (cart[itemExists].quantity >= MAX_ITEMS) return
       const updatedCart = [...cart]
       updatedCart[itemExists].quantity++
       setCart(updatedCart);
@@ -22,14 +31,55 @@ function App() {
       setCart([...cart, item])
     }
 
+    localStorage();
+
   }
+
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item, //!MANTIENE EL RESTO DE LAS PROPIEDADES
+          quantity: item.quantity + 1 //!LA CANTIDAD ES LO UNICO QUE CAMBIA
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item, //!MANTIENE EL RESTO DE LAS PROPIEDADES
+          quantity: item.quantity - 1 //!LA CANTIDAD ES LO UNICO QUE CAMBIA
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
+
+
 
   return (
     <>
 
-      <Header 
-      cart={cart}
-      
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
       />
 
       <main className="container-xl mt-5">
