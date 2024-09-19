0c128be69f29700565d1da8a92298513815b6ca7
LuisVazquezPerez <luiscarlosvazquezperez@gmail.com>
2024-09-19 12:46:59 -0800

    Proyecto terminado HOOKS StateEffectMemo

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import { db } from './data/db'
 
 function App() {
 
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([]) //!ALMACENANDO TODO EN CART
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart'); //? OBTENEMOS EL STRING
+    return localStorageCart ? JSON.parse(localStorageCart) : []  //? CONVERTIRLO EN ARREGLO
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart) 
 
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
+    localStorage.setItem('cart', JSON.stringify(cart)) //? EN EL LS NO GUARDA ARREGLOS, SOLO STRINGS (CONVERTIMOS A STRING)
   }, [cart])
 
 
